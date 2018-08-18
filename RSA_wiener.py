291940b4d44cc1ae9b44932f8a10a314264a1aab
X-Vector <30379192+X-Vector@users.noreply.github.com>
2018-08-19 00:47:10 +0200

    Update RSA_wiener.py

--- RSA_wiener.py
+++ RSA_wiener.py
@@ -127,30 +127,5 @@ class Wiener(object):
         print ("[+] d = {}\n".format(self.d))
 
 Wiener(n,e)
-
-factordb = RSA.attacks.factordb(n)
-q = factordb[0]
-p = factordb[1]
-
-slowprint("\n[+] Please Wait ... \033[95m\n")
-
-from CryptoLib.RSA import RSA
-import binascii
-
-phi = (p-1)*(q-1)
-def egcd(a,b):
-    if a == 0 :
-        return(b,0,1)
-    else:
-        g,y,x = egcd(b%a,a)
-        return (g,x-(b/a)*y,y)
-def modinv(a,m):
-    g,x,y = egcd(a,m)
-    if g != 1:
-        raise Expection("RSA Hello")
-    else :
-        return x%m
-
-d = modinv(e,phi)
-decode = pow(c,d,n)
-output = hex(decode)[2:].replace('L','')
+slowprint("\n[+] If You Have The CipherText You Can Get PlainText By Using Attack [10] ... \033[95m")
+slowprint("[+] Thanx For Using X-RSA Tool <3  \033[95m\n")
