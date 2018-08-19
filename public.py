3c5cc5b8c325adb9f4e20950463b7010c9b7e6cd
X-Vector <30379192+X-Vector@users.noreply.github.com>
2018-08-19 17:18:25 +0200

    Update public.py

--- public.py
+++ public.py
@@ -70,80 +70,83 @@ import argparse
 from Crypto.PublicKey import RSA
 
 k = raw_input("Enter Path of Public Key : ")
-key=RSA.importKey(open(k, 'r').read())
-n = key.n
-e = key.e
+try:
+    key=RSA.importKey(open(k, 'r').read())
+    slowprint("[+] Please Wait ... ")
+    n = key.n
+    e = key.e
 
-from CryptoLib.RSA import RSA
-import binascii
-
-slowprint("[+] Please Wait ... ")
-
-factordb = RSA.attacks.factordb(n)
-q = factordb[0]
-p = factordb[1]
-
-slowprint("[+] Getting n,p,q,d,e ... ")
+    from CryptoLib.RSA import RSA
+    import binascii
 
 
-print("\n[+] n = {}".format(str(n)))
-print("\n[+] e = {}".format(str(e)))
-print("\n[+] p = {}".format(str(p)))
-print("\n[+] q = {}".format(str(q)))
+    factordb = RSA.attacks.factordb(n)
+    q = factordb[0]
+    p = factordb[1]
 
-import random
-import Crypto.PublicKey.RSA
-class Wiener(object):
-    def accueil(self):
-        slowprint("")
+    slowprint("[+] Getting n,p,q,d,e ... ")
 
-    def division_euclidienne(self, a, b):
-      return (a // b, a % b)
-    def fraction_continue(self, n, d):
-      developpement = []
-      a = n
-      b = d
-      while b != 0:
-        (q,r) = self.division_euclidienne(a,b)
-        developpement.append(q)
-        a = b
-        b = r
-      return (developpement)
-    def reduites_fraction_continue(self, a):
-      l=len(a)
-      reduites=[]
-      h0 = 1
-      h1 = 0
-      k0 = 0
-      k1 = 1
-      count = 0
-      while count < l:
-        h = a[count] * h1 + h0
-        h0 = h1
-        h1 = h
-        k = a[count] * k1 + k0
-        k0 = k1
-        k1 = k
-        reduites.append((k,h))
-        count += 1
-      return (reduites)
-    def wiener(self, n, e):
-      fc = self.fraction_continue(e, n)
-      reduites = self.reduites_fraction_continue(fc)
-      message_clair = random.randint(10**1,10**5)
-      message_chiffre = pow(message_clair, e, n)
-      l = len(reduites)
-      i = 0
-      while i < l and pow(message_chiffre, reduites[i][1], n) != message_clair:
-        i += 1
-      if i != l:
-        return (reduites[i][1])
-      else:
-        print("\t[-] This RSA public key isn't a valid candidate to a Wiener Attack\n")
-        exit(0)
-    def __init__(self, n, e):
-        self.accueil()
-        self.d = self.wiener(n, e)
-        print ("[+] d = {}".format(self.d))
 
-Wiener(n,e)
+    print("\n[+] n = {}".format(str(n)))
+    print("\n[+] e = {}".format(str(e)))
+    print("\n[+] p = {}".format(str(p)))
+    print("\n[+] q = {}".format(str(q)))
+
+    import random
+    import Crypto.PublicKey.RSA
+    class Wiener(object):
+        def accueil(self):
+            slowprint("")
+
+        def division_euclidienne(self, a, b):
+          return (a // b, a % b)
+        def fraction_continue(self, n, d):
+          developpement = []
+          a = n
+          b = d
+          while b != 0:
+            (q,r) = self.division_euclidienne(a,b)
+            developpement.append(q)
+            a = b
+            b = r
+          return (developpement)
+        def reduites_fraction_continue(self, a):
+          l=len(a)
+          reduites=[]
+          h0 = 1
+          h1 = 0
+          k0 = 0
+          k1 = 1
+          count = 0
+          while count < l:
+            h = a[count] * h1 + h0
+            h0 = h1
+            h1 = h
+            k = a[count] * k1 + k0
+            k0 = k1
+            k1 = k
+            reduites.append((k,h))
+            count += 1
+          return (reduites)
+        def wiener(self, n, e):
+          fc = self.fraction_continue(e, n)
+          reduites = self.reduites_fraction_continue(fc)
+          message_clair = random.randint(10**1,10**5)
+          message_chiffre = pow(message_clair, e, n)
+          l = len(reduites)
+          i = 0
+          while i < l and pow(message_chiffre, reduites[i][1], n) != message_clair:
+            i += 1
+          if i != l:
+            return (reduites[i][1])
+          else:
+            print("\t[-] This RSA public key isn't a valid candidate to a Wiener Attack\n")
+            exit(0)
+        def __init__(self, n, e):
+            self.accueil()
+            self.d = self.wiener(n, e)
+            print ("[+] d = {}".format(self.d))
+    Wiener(n,e)
+    slowprint("\n[+] Thanx For Using X-RSA Tool <3  \033[95m\n")
+except:
+    slowprint("[-] False Attack !! ")
