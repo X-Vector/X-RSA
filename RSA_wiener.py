df7e67ebb13da1494c7a678ee4b3939de004c7ee
X-Vector <30379192+X-Vector@users.noreply.github.com>
2018-12-09 21:42:37 +0200

    Update RSA_wiener.py

--- RSA_wiener.py
+++ RSA_wiener.py
@@ -2,8 +2,9 @@ import sys
 import platform,os
 from urllib2 import *
 from platform import system
+
 def slowprint(s):
-    for c in s + '\n':
+    for c in s :
         sys.stdout.write(c)
         sys.stdout.flush()
         time.sleep(4. / 100)
@@ -56,7 +57,7 @@ _____       ________________                  _____      _____
  |    |   |    | |     | | |     || |   ||    | |    | |     | |     |
  |____|   |____| |_____|/ \|_____| \|___||____|/      \|_____|\|_____|
   %s%s
-[ Version : 0.1 ]\033[92m
+[ Version : 0.2 ]\033[92m
 [ Author  : X-Vector ]\033[96m
 [ Github  : github.com/X-Vector ]\033[93m
 [ Twitter : twitter.com/@XVector11 ]\033[95m
@@ -66,83 +67,71 @@ _____       ________________                  _____      _____
     """ % (R, W,R))
 banner()
 
-#Example : 
-#n = 744818955050534464823866087257532356968231824820271085207879949998948199709147121321290553099733152323288251591199926821010868081248668951049658913424473469563234265317502534369961636698778949885321284313747952124526309774208636874553139856631170172521493735303157992414728027248540362231668996541750186125327789044965306612074232604373780686285181122911537441192943073310204209086616936360770367059427862743272542535703406418700365566693954029683680217414854103
-#e = 57595780582988797422250554495450258341283036312290233089677435648298040662780680840440367886540630330262961400339569961467848933132138886193931053170732881768402173651699826215256813839287157821765771634896183026173084615451076310999329120859080878365701402596570941770905755711526708704996817430012923885310126572767854017353205940605301573014555030099067727738540219598443066483590687404131524809345134371422575152698769519371943813733026109708642159828957941
-#c = 305357304207903396563769252433798942116307601421155386799392591523875547772911646596463903009990423488430360340024642675941752455429625701977714941340413671092668556558724798890298527900305625979817567613711275466463556061436226589272364057532769439646178423063839292884115912035826709340674104581566501467826782079168130132642114128193813051474106526430253192254354664739229317787919578462780984845602892238745777946945435746719940312122109575086522598667077632
+#Example :
+"""
+n = 744818955050534464823866087257532356968231824820271085207879949998948199709147121321290553099733152323288251591199926821010868081248668951049658913424473469563234265317502534369961636698778949885321284313747952124526309774208636874553139856631170172521493735303157992414728027248540362231668996541750186125327789044965306612074232604373780686285181122911537441192943073310204209086616936360770367059427862743272542535703406418700365566693954029683680217414854103
+e = 57595780582988797422250554495450258341283036312290233089677435648298040662780680840440367886540630330262961400339569961467848933132138886193931053170732881768402173651699826215256813839287157821765771634896183026173084615451076310999329120859080878365701402596570941770905755711526708704996817430012923885310126572767854017353205940605301573014555030099067727738540219598443066483590687404131524809345134371422575152698769519371943813733026109708642159828957941
+c = 305357304207903396563769252433798942116307601421155386799392591523875547772911646596463903009990423488430360340024642675941752455429625701977714941340413671092668556558724798890298527900305625979817567613711275466463556061436226589272364057532769439646178423063839292884115912035826709340674104581566501467826782079168130132642114128193813051474106526430253192254354664739229317787919578462780984845602892238745777946945435746719940312122109575086522598667077632
 
-
+"""
 
 n = input(">>> n = ")
 e = input(">>> e = ")
+c = input(">>> c = ")
+
 slowprint("\n[+] Please Wait ... \033[95m\n")
 
 import random
-import Crypto.PublicKey.RSA
-class Wiener(object):
-    def accueil(self):
-        slowprint("[+] Getting d ... ")
 
-    def division_euclidienne(self, a, b):
-      return (a // b, a % b)
-    def fraction_continue(self, n, d):
-      developpement = []
-      a = n
-      b = d
-      while b != 0:
-        (q,r) = self.division_euclidienne(a,b)
-        developpement.append(q)
-        a = b
-        b = r
-      return (developpement)
-    def reduites_fraction_continue(self, a):
-      l=len(a)
-      reduites=[]
-      h0 = 1
-      h1 = 0
-      k0 = 0
-      k1 = 1
-      count = 0
-      while count < l:
-        h = a[count] * h1 + h0
-        h0 = h1
-        h1 = h
-        k = a[count] * k1 + k0
-        k0 = k1
-        k1 = k
-        reduites.append((k,h))
-        count += 1
-      return (reduites)
-    def wiener(self, n, e):
-      fc = self.fraction_continue(e, n)
-      reduites = self.reduites_fraction_continue(fc)
-      message_clair = random.randint(10**1,10**5)
-      message_chiffre = pow(message_clair, e, n)
-      l = len(reduites)
-      i = 0
-      while i < l and pow(message_chiffre, reduites[i][1], n) != message_clair:
-        i += 1
-      if i != l:
-        return (reduites[i][1])
-      else:
-        print("\t[-] This RSA public key isn't a valid candidate to a Wiener Attack\n")
-        exit(0)
-    def __init__(self, n, e):
-        self.accueil()
-        self.d = self.wiener(n, e)
-        print ("[+] d = {}\n".format(self.d))
+def division_euclidienne(a, b):
+  return (a // b, a % b)
+def fraction_continue(n, d):
+  developpement = []
+  a = n
+  b = d
+  while b != 0:
+    (q,r) = division_euclidienne(a,b)
+    developpement.append(q)
+    a = b
+    b = r
+  return (developpement)
+def reduites_fraction_continue(a):
+  l=len(a)
+  reduites=[]
+  h0 = 1
+  h1 = 0
+  k0 = 0
+  k1 = 1
+  count = 0
+  while count < l:
+    h = a[count] * h1 + h0
+    h0 = h1
+    h1 = h
+    k = a[count] * k1 + k0
+    k0 = k1
+    k1 = k
+    reduites.append((k,h))
+    count += 1
+  return (reduites)
+def wiener(n, e):
+  fc = fraction_continue(e, n)
+  reduites = reduites_fraction_continue(fc)
+  message_clair = random.randint(10**1,10**5)
+  message_chiffre = pow(message_clair, e, n)
+  l = len(reduites)
+  i = 0
+  while i < l and pow(message_chiffre, reduites[i][1], n) != message_clair:
+    i += 1
+  if i != l:
+    slowprint("[+] Getting d ... \033[95m\n")
+    return (reduites[i][1])
+  else:
+    print("[-] Sorry it's Not Wiener Attack\n")
+    exit(0)
 
-Wiener(n,e)
-
-x = raw_input("Enter [yes / no] To Get PlainText from CipherText : ")
-if x == "yes" or x == "y":
-    c = input(">>> c = ")
-    d = input(">>> d = ")
-    slowprint("\n[+] Please Wait ... \033[95m\n")
-    decode = pow(c,d,n)
-    output = (hex(decode)[2:].replace('L','')).decode("hex")
-    slowprint("[+] The PlainText  = \033[95m")
-    print(output)
-    slowprint("\n[+] Thanx For Using X-RSA Tool <3  \033[95m\n")
-else:
-    slowprint("[+] Thanx For Using X-RSA Tool <3  \033[95m\n")
+d = wiener(n,e)
+slowprint("[+] Getting PlainText ... \033[95m\n")
+decode = pow(c,d,n)
+output = (hex(decode)[2:].replace('L','')).decode("hex")
+slowprint("[+] The PlainText = \033[95m")
+print output
