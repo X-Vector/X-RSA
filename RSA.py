0045ea786797edd0e0a11a86125efb329d0e7569
X-Vector <30379192+X-Vector@users.noreply.github.com>
2018-12-09 21:42:08 +0200

    Update RSA.py

--- RSA.py
+++ RSA.py
@@ -9,7 +9,7 @@ import platform,os
 from urllib2 import *
 from platform import system
 def slowprint(s):
-    for c in s + '\n':
+    for c in s :
         sys.stdout.write(c)
         sys.stdout.flush()
         time.sleep(4. / 100)
@@ -62,7 +62,7 @@ _____       ________________                  _____      _____
  |    |   |    | |     | | |     || |   ||    | |    | |     | |     |
  |____|   |____| |_____|/ \|_____| \|___||____|/      \|_____|\|_____|
   %s%s
-[ Version : 0.1 ]\033[92m
+[ Version : 0.2 ]\033[92m
 [ Author  : X-Vector ]\033[96m
 [ Github  : github.com/X-Vector ]\033[93m
 [ Twitter : twitter.com/@XVector11 ]\033[95m
@@ -71,45 +71,68 @@ _____       ________________                  _____      _____
 
     """ % (R, W,R))
 banner()
-from CryptoLib.RSA import RSA
-import binascii
 
-# Example :
-# c = 194048013822218245260658018019940874060627700835842604475987702337533801266490182061968998210807564778328557627772974110046885380635225974269865976518335375789734689098164529086561756412074742698644530189076800227300946408167039318949544794351233987752575608106800908043533012088081995031010618521695843625062
-# n = 248501410365662412791489552646042256782092770118253438700194718631291036762726489658495565276550205113648626040596191969135846656414394584577305526761671104277390765264806022908497647300596494542202565022133435383403344333672279722534625284520459706609569974491538689429548817677759350947931780871046796607829
-# e = 65537
+import binascii
+import gmpy2
+
+
+
+# Example 1 :
+#c = 194048013822218245260658018019940874060627700835842604475987702337533801266490182061968998210807564778328557627772974110046885380635225974269865976518335375789734689098164529086561756412074742698644530189076800227300946408167039318949544794351233987752575608106800908043533012088081995031010618521695843625062
+#n = 248501410365662412791489552646042256782092770118253438700194718631291036762726489658495565276550205113648626040596191969135846656414394584577305526761671104277390765264806022908497647300596494542202565022133435383403344333672279722534625284520459706609569974491538689429548817677759350947931780871046796607829
+#e = 65537
+
+# Example 2 :
+#n = 95927091022214131235859313618337025685657931350656500630264132698389852599510198358308813869652616688807245375001990146888881030682552800587836982145527192151998189057703804830882536718184311723290034393819853924918814439601355616561237190273205228499357454477829605938284765657202538703493014172555332956153
+#e = 3
+#c = 74802199268254280440493690700608296874229186682164386879225634595063352871356558051619716745745659385094914838131944342112615526475056433710891149995541243955498341403563518984577892267288643941
 
 
 c = input(">>> c = ")
 n = input(">>> n = ")
 e = input(">>> e = ")
 
-slowprint("\n[+] Please Wait ... \033[95m\n")
+
+gmpy2.get_context().precision=200
+m = gmpy2.iroot(c, e)[0]
 try:
-    factordb = RSA.attacks.factordb(n)
-    q = factordb[0]
-    p = factordb[1]
-    phi = (p-1)*(q-1)
-    def egcd(a,b):
-        if a == 0 :
-            return(b,0,1)
-        else:
-            g,y,x = egcd(b%a,a)
-            return (g,x-(b/a)*y,y)
-    def modinv(a,m):
-        g,x,y = egcd(a,m)
-        if g != 1:
-            raise Expection("RSA Hello")
-        else :
-            return x%m
+    if pow(m,e) == c:
+        slowprint("\n[+] Please Wait ... \033[95m\n")
+        def hex_pair(x):
+            return ('0' * (len(x) % 2)) + x
+        m_hex = '{:x}'.format(m)
+        m_hex = hex_pair(m_hex)
+        msg = binascii.unhexlify(m_hex)
+        slowprint("[+] The PlainText = ")
+        print(msg.decode())
+    else:
+        slowprint("\n[+] Please Wait ... \033[95m\n")
+        factordb = RSA.attacks.factordb(n)
+        q = factordb[0]
+        p = factordb[1]
+        phi = (p-1)*(q-1)
+        def egcd(a,b):
+            if a == 0 :
+                return(b,0,1)
+            else:
+                g,y,x = egcd(b%a,a)
+                return (g,x-(b/a)*y,y)
 
-    d = modinv(e,phi)
-    decode = pow(c,d,n)
-    output = (hex(decode)[2:].replace('L','')).decode("hex")
+        def modinv(a,m):
+            g,x,y = egcd(a,m)
+            if g != 1:
+                raise Expection("RSA Hello")
+            else :
+                return x%m
 
+        d = modinv(e,phi)
+        decode = pow(c,d,n)
+        output = (hex(decode)[2:].replace('L','')).decode("hex")
+        slowprint("[+] The PlainText = ")
+        print(output)
 
-
-    slowprint("[+] The PlainText = ")
-    print(output)
+except IndexError:
+    slowprint("[-] Sorry Can't Factorize n :( ")
+    slowprint("\n[!] Try To Use MultiPrime Attack ")
 except:
     slowprint("[-] False Attack !! ")
