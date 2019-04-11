2728778644a9e3418682581e2ce6f32b7277f0a5
X-Vector <30379192+X-Vector@users.noreply.github.com>
2019-04-11 17:00:20 +0200

    Update RSA8.py

--- RSA8.py
+++ RSA8.py
@@ -65,16 +65,17 @@ _____       ________________                  _____      _____
     """ % (R, W,R))
 banner()
 
-import gmpy2
-import binascii
 
-gmpy2.get_context().precision=99999999999999999
 
 # Example
 #c = 74802199268254280440493690700608296874229186682164386879225634595063352871356558051619716745745659385094914838131944342112615526475056433710891149995541243955498341403563518984577892267288643941
-c = input(">>> c = ")
-slowprint("\n[+] Please Wait ... \033[95m\n")
+
 try:
+    import gmpy2
+    import binascii
+    gmpy2.get_context().precision=99999999999999999
+    c = int(raw_input(">>> c = "))
+    slowprint("\n[+] Please Wait ... \033[95m\n")
     m = gmpy2.iroot(c, 3)[0]
     assert pow(m,3) == c
     def hex_pair(x):
@@ -84,5 +85,13 @@ try:
     msg = binascii.unhexlify(m_hex)
     slowprint("[+] The PlainText = ")
     print(msg.decode())
+except ImportError:
+    slowprint("\n[-] Module Not Setup")
+except ValueError:
+    slowprint("\n[-] c,p,q,e Must Be Integar Number")
+except AssertionError:
+    slowprint("\n[-] Wrong Data")
+except KeyboardInterrupt:
+    exit()
 except:
-    slowprint("[-] False Attack !! ")
+    slowprint("\n[-] False Attack !")
