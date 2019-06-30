f596c0c43d68b8eedc4746c447389d69cb4f8e75
Mohamed Abdelfatah <algazarbas@gmail.com>
2019-06-30 22:15:01 +0200

    Update Attack.py

--- Attack.py
+++ Attack.py
@@ -24,6 +24,7 @@ elif check == 1:
 [11] - Attack(c,d,n,e)\033[92m
 [12] - Attack(c,n,e) \033[93m                   [ Multi Prime Number ]
 [13] - Attack(c,e = 3)\033[0m
+[14] - Attack(c1,c2,n1,n2,e) \033[96m            [ Common Exponent ]
 [0] - Exit \033[92m
         """)
     x = int(raw_input(">>> "))
@@ -66,6 +67,9 @@ elif check == 1:
     elif x == 13:
         os.system(clear)
         import RSA8
+    elif x == 14:
+        os.system(clear)
+        import RSA_common_exponent
     else:
         exit()
 else:
